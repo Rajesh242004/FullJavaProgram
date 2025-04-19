7da8972c61356e212bc2ff19aa2c614f2d9643df
Rajesh <rajeshsathish24@gmail.com>
2025-04-19 08:41:27 +0530

    Basic programs

---
+++ Basic.java
@@ -0,0 +1,40 @@
+class Basic{
+    public static void main(String args[]){
+        int num1 = 10;
+        int num2 = 20;
+        int result = num1+num2;
+        System.out.println(result);
+
+        double n1 = 2.2;
+        double n2 = 4.4;
+        double r1 = n1+n2;
+        System.out.println(r1);
+
+        float a1 = 2.2f;
+        float a2 =  3.3f;
+        float r2 = a1+a2;
+        System.out.println(r2);
+
+        char b1 = 'a';
+        char b2 = 'b';
+        System.out.println(b1+b2);
+
+        String user1 = "java";
+        String user2 = "program";
+        System.out.println(user1 + user2);
+
+        int c1 = 3;
+        int c2 = 5;
+        boolean c3 = c1>c2;
+        System.out.println(c3);
+
+        int number1 = 2;
+        number1++;
+        int number2 = 4;
+        number2+=3;
+        System.out.println(number1+number2);
+        
+
+
+    }
+}
